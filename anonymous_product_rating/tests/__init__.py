4168f8630e1c8bd9fa466eaaafd5bbff4c9022a8
Isidoro Roa <37345629+zapateroremendon@users.noreply.github.com>
2026-08-11 22:34:23 -0400

    Delete anonymous_product_rating directory

--- anonymous_product_rating/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import test_anonymous_rating
-from . import test_user_experience
-from . import test_serialization_protection
-from . import test_website_published_field
-from . import test_website_published_critical
-from . import test_cron_serialization_fix
